3ce1e8c43a07855a7b7171c432db2f9fea2bcb9c
Roman-V-N <r.v.nazarov@ya.ru>
2024-04-08 00:24:13 +0300

    5 commit Спринт 8 : Тема 5 : Урок 6

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -12,13 +12,11 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         val searchButton = findViewById<Button>(R.id.search)
         val searchClickListener: View.OnClickListener = object : View.OnClickListener{
             override fun onClick(v: View?) {
-              // Спринт 8/25 → Тема 4/6: Работа в коде → Урок 2/2:  Toast.makeText(this@MainActivity,"Нажата клавиша \"Поиск\"", Toast.LENGTH_SHORT).show()
                 val searchIntent = Intent(this@MainActivity,SearchActivity::class.java)
                 startActivity(searchIntent)
             }
@@ -27,14 +25,12 @@ class MainActivity : AppCompatActivity() {
 
         val libraryButton = findViewById<Button>(R.id.library)
         libraryButton.setOnClickListener{
-           // Спринт 8/25 → Тема 4/6: Работа в коде → Урок 2/2: Toast.makeText(this@MainActivity,"Нажата клавиша \"Медиатека\"", Toast.LENGTH_SHORT).show()
             val libraryIntent = Intent(this, LibraryActivity::class.java)
             startActivity(libraryIntent)
         }
 
         val settingsButton = findViewById<Button>(R.id.settings)
         settingsButton.setOnClickListener{
-          // Спринт 8/25 → Тема 4/6: Работа в коде → Урок 2/2: Toast.makeText(this@MainActivity,"Нажата клавиша \"Настройки\"", Toast.LENGTH_SHORT).show()
             val  settingsIntent = Intent(this, SettingsActivity::class.java)
             startActivity(settingsIntent)
         }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -11,7 +11,6 @@ import androidx.appcompat.app.AppCompatActivity
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
 
 
