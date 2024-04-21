f90469056800ce0fe84c52c3aa0e316e16775044
Roman-V-N <r.v.nazarov@ya.ru>
2024-04-21 09:47:57 +0300

    Commit Спринт 9 : Тема 3 : Урок 3

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,6 +1,71 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
-}
+
+    var editValue : String = ""
+
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+
+        val backButton = findViewById<ImageView>(R.id.back_main)
+        val clearButton = findViewById<ImageView>(R.id.clear_button)
+        val searchEdit =  findViewById<EditText>(R.id.search_edit)
+
+        searchEdit.requestFocus()
+
+
+        backButton.setOnClickListener {
+            super.finish()
+        }
+
+        clearButton.setOnClickListener {
+            searchEdit.setText("")
+            val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(searchEdit.getWindowToken(), 0)
+        }
+
+
+        val editTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                editValue = s.toString()
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        searchEdit.addTextChangedListener(editTextWatcher)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+
+
+
+    }
+
